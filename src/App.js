d37ef8c14c222f04f2f02c697bfbda430f773a57
leslieshea <lshea@audigy.com>
2023-09-05 14:28:41 -0700

    Added routing to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
 
 export default function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Route path="/" exact component={Home} />
+      <Route path="/profile" component={Profile} />
+    </>
   );
 }
