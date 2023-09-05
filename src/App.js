e8d86eae4494f2a9a40aa4ae601fed266401db73
leslieshea <lshea@audigy.com>
2023-09-05 14:31:58 -0700

    Added basic navigation between home and profile pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
+import Nav from './Nav';
 
 export default function App() {
   return (
     <>
+    <Nav />
       <Route path="/" exact component={Home} />
       <Route path="/profile" component={Profile} />
     </>
