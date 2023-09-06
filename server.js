1d919cafd5ecd1454ecb7569117751a3fff56cdf
leslieshea <lshea@audigy.com>
2023-09-06 16:43:00 -0700

    Updated start script to start client and server

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/public', function(request, response) {
 });
 
 app.listen(3001);
-console.log("API sever listening on " + process.env.REACT_APP_AUTH0_API_URL);
+console.log("API server listening on " + process.env.REACT_APP_AUTH0_API_URL);
