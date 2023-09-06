9f8fd27d70ed7eea1af156a4d1b210b4ed6a4176
leslieshea <lshea@audigy.com>
2023-09-06 12:02:53 -0700

    Added user profile data to Profile page

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -3,6 +3,7 @@ import auth0 from 'auth0-js';
 export default class Auth {
 	constructor(history) {
 		this.history = history;
+		this.userProfile = null;
 		this.auth0 = new auth0.WebAuth({
 			domain: process.env.REACT_APP_AUTH0_DOMAIN,
 			clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -48,5 +49,35 @@ export default class Auth {
 		const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
 
 		return new Date().getTime() < expiresAt;
-	}
+	};
+
+	logout = () => {
+		localStorage.removeItem("access_token");
+		localStorage.removeItem("id_token");
+		localStorage.removeItem("expires_at");
+		this.userProfile = null;
+		
+		this.auth0.logout({
+			clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+			returnTo: "http://localhost:3000"
+		});
+	};
+
+	getAccessToken = () => {
+		const accessToken = localStorage.getItem("access_token");
+
+		if (!accessToken) {
+			throw new Error ("No access token found.");
+		}
+
+		return accessToken;
+	};
+
+	getProfile = callback => {
+		if (this.userProfile) return callback(this.userProfile);
+		this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+			if (profile) this.userProfile = profile;
+			callback(profile, err);
+		});
+	};
 }
