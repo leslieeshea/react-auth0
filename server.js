571c1c5c030a6e670ba8695ac3c76d35cf4a197a
leslieshea <lshea@audigy.com>
2023-09-06 16:39:15 -0700

    Added express for our API endpoint

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/public', function(request, response) {
+	response.json({
+		message: 'Hello from a public API!'
+	});
+});
+
+app.listen(3001);
+console.log("API sever listening on " + process.env.REACT_APP_AUTH0_API_URL);
