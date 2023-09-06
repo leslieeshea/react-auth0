aaab85325125e2e28e5942bab72f1405c9cd4d73
leslieshea <lshea@audigy.com>
2023-09-05 19:55:13 -0700

    Added authorization

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import { Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
 import Nav from './Nav';
+import Auth from './Auth/Auth';
+import Callback from './Callback';
 
-export default function App() {
+export default function App(props) {
+  const auth = new Auth(props.history);
+
   return (
     <>
     <Nav />
     <div className="body">
-      <Route path="/" exact component={Home} />
+      <Route
+        path="/" exact render={props => <Home auth={auth} {...props} />}
+      />
+      <Route
+        path="/callback" exact render={props => <Callback auth={auth} {...props} />}
+      />
       <Route path="/profile" component={Profile} />
     </div>
     </>
