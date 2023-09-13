6b3e7140ae0c0060cc5787c35517312e06d7da66
leslieshea <lshea@audigy.com>
2023-09-13 15:49:29 -0700

    Checking JWT

--- server.js
+++ server.js
@@ -3,24 +3,6 @@ require('dotenv').config();
 const jwt = require('express-jwt'); // Validate JWT and set request.user
 const jwksRsa = require('jwks-rsa'); // Retrieve RSA keys from a JSON web key set (JWKS) endpoint
 
-// const checkJwt = jwt({
-// 	/* Dynamically provide a signing key based on the kid in the header
-// 	and signing keys provided by the JWKS endpoint */
-// 	secret: jwksRsa.expressJwtSecret({
-// 		cache: true, // Cache the signing key
-// 		rateLimit: true,
-// 		jwksRequestsPerMinute: 5, // Prevent attackers from requesting more than 5 per minute
-// 		jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}}/.well-known/jwks.json`
-// 	}),
-
-// 	// Validate the audience and the issuer
-// 	audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-// 	issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
-
-// 	// This must match the algorithm selected in the Auth0 dashboard under the app's advanced settings under the 0Auth tab
-// 	algorithms: ["RS256"]
-// });
-
 const checkJwt = jwt({
 	// Dynamically provide a signing key based on the kid in the header
 	// and the signing keys provided by the JWKS endpoint.
